557bf84940eaa92bb2e57fdd2f7c0b201e922acb
vanton <vanton@qq.com>
2019-01-15 11:50:51 +0800

    no message

--- README.md
+++ README.md
@@ -1,47 +1,54 @@
 # saved
+
 > saved on git
 
+<https://vanton.github.io/saved>
 
-https://vanton.github.io/saved/
+## list
 
+### January 14, 2019
 
-
-
-
-
-### January 14, 2019 
 - [PPRows/README_CN.md at master · jkpang/PPRows](https://github.com/jkpang/PPRows/blob/master/README_CN.md) 在 Mac 上计算你写了多少行代码
-- [Free OS X and iOS Apps - ZipZapMac](https://zipzapmac.com/free) Free OS X and iOS Apps - ZipZapMac
+- [ZipZapMac](https://zipzapmac.com/free) Free OS X and iOS Apps
 - [MJML App](https://mjmlio.github.io/mjml-app/) A lightweight app that makes responsive email
-- [zsh-syntax-highlighting/INSTALL.md at master · zsh-users/zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md) zsh-syntax-highlighting/INSTALL.md at master · zsh-users/zsh-syntax-highlighting
-- [nicolargo/glances](https://github.com/nicolargo/glances) : Glances an Eye on your system. A top/htop alternative for GNU/Linux, BSD, Mac OS and Windows operating systems.
-- [jakubroztocil/httpie](https://github.com/jakubroztocil/httpie) : As easy as httpie /aitch-tee-tee-pie/ 🥧 Modern command line HTTP client – user-friendly curl alternative with intuitive UI, JSON support, syntax highlighting, wget-like downloads, extensions, etc. https://twitter.com/clihttp
-  > pip install requests urllib3  --force --upgrade && pip install httpie
+- [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md) zsh-syntax-highlighting/INSTALL.md at master
+- [nicolargo/glances](https://github.com/nicolargo/glances) : Glances an Eye on your system. A `top`/`htop` alternative for GNU/Linux, BSD, Mac OS and Windows operating systems.
+- [jakubroztocil/httpie](https://github.com/jakubroztocil/httpie) : As easy as httpie `/aitch-tee-tee-pie/` 🥧 Modern command line HTTP client – user-friendly curl alternative with intuitive UI, JSON support, syntax highlighting, wget-like downloads, extensions, etc. <https://twitter.com/clihttp>
+    > 安装报错的话需要强制更新 `requests` `urllib3`
+    ```bash
+    pip install requests urllib3  --force --upgrade && pip install httpie
+    ```
 
-### January 13, 2019 
+### January 13, 2019
+
 - [miyakogi/pyppeteer](https://github.com/miyakogi/pyppeteer) : Headless chrome/chromium automation library (unofficial port of puppeteer)
 - [ElemeFE/page-skeleton-webpack-plugin](https://github.com/ElemeFE/page-skeleton-webpack-plugin) : Webpack plugin to generate the skeleton page automatically
 
-### January 10, 2019 
+### January 10, 2019
+
 - [开始 | Rize 文档](https://rize.js.org/zh-CN/getting-started.html) 开始 | Rize 文档
 - [g-plane/rize](https://github.com/g-plane/rize) : High-level, fluent and chainable API provided library for puppeteer.
 
-### January 9, 2019 
+### January 9, 2019
+
 - [checkly/puppeteer-recorder](https://github.com/checkly/puppeteer-recorder) : Puppeteer recorder is a Chrome extension that records your browser interactions and generates a Puppeteer script.
 - [csbun/thal](https://github.com/csbun/thal) : 译文：Puppeteer 与 Chrome Headless —— 从入门到爬虫
 - [zhentaoo/puppeteer-deep](https://github.com/zhentaoo/puppeteer-deep) : Puppeteer, Headless Chrome；爬取《es6标准入门》、自动推文到掘金、站点性能分析；高级爬虫、自动化UI测试、性能分析；
 - [transitive-bullshit/awesome-puppeteer](https://github.com/transitive-bullshit/awesome-puppeteer) : A curated list of awesome puppeteer resources.
 - [awesome-puppeteer/readme.zh.md at master · transitive-bullshit/awesome-puppeteer](https://github.com/transitive-bullshit/awesome-puppeteer/blob/master/readme.zh.md) awesome-puppeteer/readme.zh.md at master · transitive-bullshit/awesome-puppeteer
 
-### January 8, 2019 
+### January 8, 2019
+
 - [whatwg/html](https://github.com/whatwg/html) : HTML Standard
 - [HTML Standard](https://whatwg-cn.github.io/html/multipage/workers.html#workers) HTML Standard
 - [使用 Web Workers | MDN](https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Workers_API/Using_web_workers) 使用 Web Workers | MDN
 
-### January 7, 2019 
+### January 7, 2019
+
 - [ziadoz/awesome-php](https://github.com/ziadoz/awesome-php) : A curated list of amazingly awesome PHP libraries, resources and shiny things.
 
-### January 3, 2019 
+### January 3, 2019
+
 - [coooold/CurlFuture](https://github.com/coooold/CurlFuture) : PHP的curl并行请求库
 - [lionsoul2014/ip2region](https://github.com/lionsoul2014/ip2region) : Ip2region is a offline IP location library with accuracy rate of 99.9% and 0.0x millseconds searching performance. DB file is less then 5Mb with all ip address stored. binding for Java,PHP,C,Python,Nodejs,Golang,C#,lua. Binary,B-tree,Memory searching algorithm
 - [easy-swoole/easyswoole](https://github.com/easy-swoole/easyswoole) : High performance Coroutine PHP Framework, base on Swoole
@@ -56,24 +63,27 @@ https://vanton.github.io/saved/
 - [SeleniumHQ/selenium](https://github.com/SeleniumHQ/selenium) : A browser automation framework and ecosystem.
 - [深入理解 call，apply 和 bind - 一像素 - 博客园](http://www.cnblogs.com/onepixel/p/5143863.html) 深入理解 call，apply 和 bind - 一像素 - 博客园
 
-### December 26, 2018 
+### December 26, 2018
+
 - [kdabir/has](https://github.com/kdabir/has) : checks presence of various command line tools and their versions on the path
 
-### December 25, 2018 
-- [Make headless Chrome and Puppeteer use proxy server with authentication](https://blog.apify.com/how-to-make-headless-chrome-and-puppeteer-use-a-proxy-server-with-authentication-249a21a79212) Make headless Chrome and Puppeteer use proxy server with authentication
-- [proxy-chain - npm](https://www.npmjs.com/package/proxy-chain) proxy-chain - npm
+### December 25, 2018
 
-### December 24, 2018 
+- [Make headless Chrome and Puppeteer use proxy server with authentication](https://blog.apify.com/how-to-make-headless-chrome-and-puppeteer-use-a-proxy-server-with-authentication-249a21a79212) Make headless Chrome and Puppeteer use proxy server with authentication
+- [proxy-chain - npm](https://www.npmjs.com/package/proxy-chain) 为需要验证的代理创建匿名代理
+
+### December 24, 2018
+
 - [tldr-pages/tldr](https://github.com/tldr-pages/tldr) : Simplified and community-driven man pages
 - [tldr-pages/tldr-node-client](https://github.com/tldr-pages/tldr-node-client#configuration) : Node.js command line client for TLDR-pages
 - [NCurses Disk Usage](https://dev.yorhel.nl/ncdu) NCurses Disk Usage
 - [ivanilves/xiringuito](https://github.com/ivanilves/xiringuito) : SSH-based "VPN for poors"
-- [jakubroztocil/httpie](https://github.com/jakubroztocil/httpie) : As easy as httpie /aitch-tee-tee-pie/ 🥧 Modern command line HTTP client – user-friendly curl alternative with intuitive UI, JSON support, syntax highlighting, wget-like downloads, extensions, etc. https://twitter.com/clihttp
 - [unixorn/awesome-zsh-plugins](https://github.com/unixorn/awesome-zsh-plugins) : A collection of ZSH frameworks, plugins & themes inspired by the various awesome list collections out there.
 - [wakatime/wakatime](https://github.com/wakatime/wakatime) : Command line interface used by all WakaTime text editor plugins.
 - [wbingli/zsh-wakatime](https://github.com/wbingli/zsh-wakatime) : ZSH plugin for wakatime
 
-### December 23, 2018 
+### December 23, 2018
+
 - [paoloantinori/hhighlighter](https://github.com/paoloantinori/hhighlighter) : A command line tool to highlight terms
 - [nicolargo/glances](https://github.com/nicolargo/glances) : Glances an Eye on your system. A top/htop alternative.
 - [allinurl/goaccess](https://github.com/allinurl/goaccess) : GoAccess is a real-time web log analyzer and interactive viewer that runs in a terminal in *nix systems or through your browser.
@@ -83,7 +93,8 @@ https://vanton.github.io/saved/
 - [traviscross/mtr](https://github.com/traviscross/mtr) : Official repository for mtr, a network diagnostic tool
 - [jlevy/procdog](https://github.com/jlevy/procdog) : Lightweight command-line process control
 
-### December 22, 2018 
+### December 22, 2018
+
 - [Cabbagec/termux-ohmyzsh](https://github.com/Cabbagec/termux-ohmyzsh) Cabbagec/termux-ohmyzsh
 - [robbyrussell/oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh) : A delightful community-driven (with 1,200+ contributors) framework for managing your zsh configuration. Includes 200+ optional plugins (rails, git, OSX, hub, capistrano, brew, ant, php, python, etc), over 140 themes to spice up your morning, and an auto-update tool so that makes it easy to keep up with the latest updates from the community.
 - [nvbn/thefuck](https://github.com/nvbn/thefuck) : Magnificent app which corrects your previous console command.
@@ -95,10 +106,12 @@ https://vanton.github.io/saved/
 - [barryclark/bashstrap](https://github.com/barryclark/bashstrap) : A quick way to spruce up your terminal in OSX.
 - [zsh-users/zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting) : Fish shell like syntax highlighting for Zsh.
 
-### December 19, 2018 
+### December 19, 2018
+
 - [GoogleChrome/puppeteer](https://github.com/GoogleChrome/puppeteer) : Headless Chrome Node API
 
-### December 18, 2018 
+### December 18, 2018
+
 - [softvar/enhanced-github](https://github.com/softvar/enhanced-github) : Chrome extension to display size of each file, download link and copy file contents directly to clipboard
 - [xxhomey19/github-file-icon](https://github.com/xxhomey19/github-file-icon) : 🌈 🗂 A browser extension which gives different filetypes different icons to GitHub, Gitlab, Bitbucket, gitea and gogs.
 - [pd4d10/octohint](https://github.com/pd4d10/octohint) : The missing IntelliSense hint for GitHub and GitLab
@@ -108,10 +121,10 @@ https://vanton.github.io/saved/
 - [violentmonkey/violentmonkey](https://github.com/violentmonkey/violentmonkey) : Violentmonkey provides userscripts support for browsers. It works on browsers with WebExtensions support.
 - [FirefoxBar/xStyle](https://github.com/FirefoxBar/xStyle) : A user styles manager for Firefox and Chrome
 - [zxlie/FeHelper](https://github.com/zxlie/FeHelper) : 😍Web前端助手--FeHelper（Chrome Extension, Front-end Toolbox, All in one!）
-
 - [fatedier/frp](https://github.com/fatedier/frp) : A fast reverse proxy to help you expose a local server behind a NAT or firewall to the internet.
 
-### December 17, 2018 
+### December 17, 2018
+
 - [Idnan/like-on-github](https://github.com/Idnan/like-on-github) : Chrome extension - Link a repository with the extension and automatically save the links to content you like
 - [sindresorhus/refined-github](https://github.com/sindresorhus/refined-github) : Browser extension that simplifies the GitHub interface and adds useful features
 - [sindresorhus/awesome](https://github.com/sindresorhus/awesome) : Curated list of awesome lists
---
+++ fbkngleiiccokoifohhjhlagkejlphkj/2.1.0_0/like-on-github.js
@@ -0,0 +1,449 @@
+/**
+ * LikeOnGitHub
+ *
+ * This file is part of the LikeOnGitHub; an opensource Google Chrome extension
+ * http://github.com/idnan/like-on-github
+ *
+ * MIT (c) Adnan Ahmed <mahradnan@hotmail.com>
+ */
+/* eslint-disable no-undef */
+
+(function () {
+
+    var Helper = {
+
+        /**
+         * Hides the log on github popup
+         */
+        closePopup: function () {
+
+            let $container = $(Config.EX_COTAINER);
+
+            if ($container.length === 0) {
+                return false;
+            }
+
+            Helper.resetPopupHtml();
+
+            $(Config.EX_COTAINER).hide();
+            $(Config.EX_INPUT_TITLE).val('');
+            $(Config.EX_INPUT_URL).val('');
+            $(Config.EX_INPUT_COMMENT).val('');
+
+            return true;
+        },
+
+        /**
+         * Gets the tab switcher element and makes it visible. If it cannot find the element creates it.
+         */
+        showPopup: function () {
+            let $container = $(Config.EX_COTAINER);
+
+            // Some pages remove the tab switcher HTML by chance
+            // so we check if the tab switcher was found and we re append if it is not found
+            if ($container.length === 0) {
+                appendLikeOnGithubHtml(Config.EX_CONTAINER_BODY);
+                $container = $(Config.EX_COTAINER);
+            }
+
+            $container.show();
+        },
+
+        /**
+         * Reset the html of the popup
+         */
+        resetPopupHtml: function () {
+
+            $(Config.EX_BTN_SAVE).text('Save');
+
+            $(Config.EX_ERROR_CONTAINER).hide();
+            $(Config.EX_ERROR_LINE).html('');
+        },
+
+        /**
+         * Validate the form fields
+         */
+        isFormValid: function () {
+
+            let title = $(Config.EX_INPUT_TITLE).val().trim(),
+                comment = $(Config.EX_INPUT_COMMENT).val().trim();
+
+            return !!(title && comment);
+        },
+    };
+
+    var Storage = {
+
+        /**
+         * Get value from storage
+         *
+         * @returns {string}
+         * @param callback
+         */
+        getRepoUrl: function (callback) {
+
+            chrome.extension.sendMessage({
+                type: 'getStorage',
+                params: {
+                    keys: ['owner', 'repo', 'path', 'token', 'committer_name', 'committer_email']
+                }
+            }, function (res) {
+                callback(res);
+            });
+        },
+    };
+
+    /**
+     * Configuration constants for the extension
+     *
+     * @type {Object}
+     */
+    var Config = {
+
+        // Templates
+        MAIN_TEMPLATE: '<div class="logh">' +
+        '<h3>Like On GitHub</h3>' +
+        '<div class="clogl">' +
+        '<div class="lbllogh"><span class="reqlogh">*</span>Title (label for the link)</div>' +
+        '<input type="text" name="title">' +
+        '</div>' +
+        '<div class="clogl">' +
+        '<input type="hidden" name="url">' +
+        '</div>' +
+        '<div class="clogl">' +
+        '<div class="lbllogh"><span class="reqlogh">*</span>Comment (commit message)</div>' +
+        '<textarea name="comment"></textarea>' +
+        '</div>' +
+        '<div id="action-btns">' +
+        '<div class="btn btn-primary" id="logh_btn_save">Save</div>' +
+        '<div class="btn" id="logh_btn_cancel">Cancel</div>' +
+        '</div>' +
+        '<div class="elogh hlogh">' +
+        '<p></p>' +
+        '</div>' +
+        '</div>',
+
+        // References to DOM elements
+        EX_COTAINER: '.logh',
+        EX_INPUT_TITLE: '.logh input[name="title"]',
+        EX_INPUT_URL: '.logh input[name="url"]',
+        EX_INPUT_COMMENT: '.logh textarea[name="comment"]',
+        EX_CONTAINER_BODY: 'body',
+        EX_BTN_SAVE: '.logh #logh_btn_save',
+        EX_BTN_CANCEL: '.logh #logh_btn_cancel',
+        EX_ERROR_CONTAINER: '.logh .elogh',
+        EX_ERROR_LINE: '.logh .elogh p',
+
+        // Shortcut for activation
+        MASTER_KEY: '⌘+⇧+l, ⌃+⇧+l',
+
+        // Key codes for certain actions
+        ESCAPE_KEY: 27,
+
+        // REPO
+        BASE_URL: 'https://api.github.com/repos',
+    };
+
+    /**
+     * Houses all the browser related actions
+     *
+     * @type {Object}
+     */
+    var ActiveTab = {
+
+        activeTab: false,
+
+        /**
+         * Return current selected tab
+         *
+         * @returns {*}
+         */
+        get: function (callback) {
+
+            let activeTab = ActiveTab.activeTab;
+
+            if (activeTab) {
+                callback(activeTab);
+            }
+
+            chrome.extension.sendMessage({type: 'getActiveTab'}, function (tab) {
+
+                if (!tab) {
+                    return false;
+                }
+
+                ActiveTab.activeTab = tab;
+
+                callback(ActiveTab.activeTab);
+            });
+        }
+    };
+
+    var Repo = {
+
+        commitLike: function (items) {
+
+            let repoUrl = `${Config.BASE_URL}/${items['owner']}/${items['repo']}/contents/${items['path']}?access_token=${items['token']}`;
+
+            if (!repoUrl) {
+                return false;
+            }
+
+            let activeTabTitle = $(Config.EX_INPUT_TITLE).val(),
+                activeTabUrl = $(Config.EX_INPUT_URL).val(),
+                commitMessage = $(Config.EX_INPUT_COMMENT).val();
+
+            fetch(repoUrl)
+                .then(response => response.json())
+                .then(response => {
+
+                    let sha = response.sha,
+                        encodedContent = response.content,
+                        decodedContent = decodeURIComponent(escape(window.atob(encodedContent)));
+
+                    const title = activeTabTitle.split(':')[0];
+                    const desc = activeTabTitle.substring(activeTabTitle.indexOf(':'));
+                    const dataToAppend = `- [${title}](${activeTabUrl}) ${desc}\n`;
+                    // const dataToAppend = `- [${activeTabTitle}](${activeTabUrl}) \n`;
+
+                    // append data in the front
+                    decodedContent = Repo.appendDataBefore(dataToAppend, decodedContent);
+
+                    // decode content
+                    encodedContent = window.btoa(unescape(encodeURIComponent(decodedContent)));
+
+                    // prepare commit
+                    return {
+                        sha: sha,
+                        content: encodedContent,
+                        message: commitMessage,
+                        committer: {
+                            'name': items['committer_name'],
+                            'email': items['committer_email']
+                        }
+                    };
+                })
+                .then(commit => fetch(repoUrl, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(commit)
+                }))
+                .then(success => {
+
+                    $(Config.EX_BTN_SAVE).text('Save');
+
+                    success.json().then((res) => {
+
+                        $(Config.EX_ERROR_CONTAINER).show();
+
+                        let message = '';
+                        if (success.status && success.status === 200) {
+                            message = 'Link pushed: <a href="' + res.commit.html_url + '" target="_blank">Click</a> (Popup will auto close after 5 secs)';
+
+                            // close the popup after 5 secs
+                            setTimeout(function () {
+                                Helper.closePopup();
+                            }, 5000);
+                        } else {
+                            message = res.message;
+                        }
+
+                        $(Config.EX_ERROR_LINE).html(message);
+                        $(Config.EX_BTN_SAVE).removeClass('saving');
+                    });
+                })
+                .catch(error => {
+                    $(Config.EX_BTN_SAVE).removeClass('saving').text('Save');
+                });
+        },
+
+        /**
+         * Append data in the front
+         * @param dataToAppend
+         * @param content
+         * @returns {String}
+         */
+        appendDataBefore: function (dataToAppend, content) {
+            // If the file is empty
+            if (content.trim().length === 0) {
+                content += '# today-i-liked \nContent that I liked. Saved using https://goo.gl/Wj595G \n';
+            }
+            const arr = content.split('###');
+            // if the length of arr is 1, then it is the first time to append data
+            if (arr.length === 1) {
+                arr[0] += Repo.getDateHeader();
+                arr[0] += dataToAppend;
+                content = arr.join('');
+            } else {
+                // if has not append data of currentDate, then append DateHeader
+                if (!Repo.isCurrentDateExists(content)) {
+                    arr[0] += Repo.getDateHeader();
+                    arr[0] += dataToAppend + '\n';
+                } else {
+                    // if already have date then append to that
+                    // 日期前添加空行，防止格式化后标题失效
+                    // 删除结尾的多个空行
+                    arr[1] = arr[1].trimEnd('\n') + '\n';
+                    arr[1] += dataToAppend + '\n';
+                }
+                content = arr.join('###');
+            }
+            return content;
+        },
+
+        /**
+         * Return date header
+         *
+         * @returns {string}
+         */
+        getDateHeader: function () {
+            // 标题建议前后各有一个空行
+            return `\n### ${Repo.getCurrentDate()} \n\n`;
+        },
+
+        /**
+         * Check if current date already exists in the content
+         *
+         * @param content
+         * @returns {boolean}
+         */
+        isCurrentDateExists: function (content) {
+            return (content.indexOf(Repo.getCurrentDate()) !== -1);
+        },
+
+        /**
+         * Return current
+         *
+         * @returns {string}
+         */
+        getCurrentDate: function () {
+            const date = new Date();
+            return `${Repo.monthNames()[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+        },
+
+        /**
+         * Return month names
+         *
+         * @returns {string[]}
+         */
+        monthNames: function () {
+            return [
+                'January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'
+            ];
+        },
+    };
+
+    /**
+     * Main extension class
+     *
+     * @returns {{loadExtension: loadExtension, bindUI: bindUI}}
+     * @constructor
+     */
+    function LikeOnGithub() {
+
+        /**
+         * Appends the tab switcher HTML to the $container
+         *
+         * @param $container
+         * @returns {*}
+         */
+        function appendLikeOnGithubHtml($container) {
+            if (!($container instanceof jQuery)) {
+                $container = $($container);
+            }
+
+            $container.append(Config.MAIN_TEMPLATE);
+            return $container;
+        }
+
+        return {
+
+            /**
+             * Loads the extension in specified container
+             *
+             * @param $container
+             */
+            loadExtension: function ($container) {
+                appendLikeOnGithubHtml($container);
+                this.bindUI();
+            },
+
+            /**
+             * Binds the UI elements for the extension
+             */
+            bindUI: function () {
+
+                // close on escape key
+                $(document).on('keyup', function (e) {
+                    if (e.keyCode === Config.ESCAPE_KEY) {
+                        Helper.closePopup();
+                    }
+                });
+
+                // if clicked outside the popup
+                $(document).on('mouseup', function (e) {
+                    let container = $(Config.EX_COTAINER);
+
+                    if (!container.is(e.target) && container.has(e.target).length === 0) {
+                        Helper.closePopup();
+                    }
+                });
+
+                // hide the switcher on blurring of input
+                $(document).on('click', Config.EX_BTN_CANCEL, function () {
+                    Helper.closePopup();
+                });
+
+                // hide the switcher on blurring of input
+                $(document).on('click', Config.EX_BTN_SAVE, function () {
+
+                    let saveBtn = $(Config.EX_BTN_SAVE);
+
+                    if (saveBtn.hasClass('saving') || !Helper.isFormValid()) {
+                        return false;
+                    }
+
+                    Helper.resetPopupHtml();
+
+                    saveBtn.addClass('saving').text('Saving...');
+
+                    Storage.getRepoUrl(function (items) {
+
+                        // commit the like
+                        if (items) {
+                            Repo.commitLike(items);
+                        }
+                    });
+                });
+
+                // master key binding for which extension will be enabled
+                key(Config.MASTER_KEY, function () {
+                    Helper.showPopup();
+
+                    $(Config.EX_INPUT_TITLE).focus();
+
+                    // get the active tab
+                    ActiveTab.get(function (activeTab) {
+
+                        if (!activeTab) {
+                            return false;
+                        }
+
+                        $(Config.EX_INPUT_TITLE).val(activeTab.title);
+                        $(Config.EX_INPUT_URL).val(activeTab.url);
+                        $(Config.EX_INPUT_COMMENT).val('New Link: ' + activeTab.title);
+                    });
+                });
+            }
+        };
+    }
+
+    $(document).ready(function () {
+        var likeOnGithub = new LikeOnGithub();
+        likeOnGithub.loadExtension(Config.EX_CONTAINER_BODY);
+    });
+
+})();
